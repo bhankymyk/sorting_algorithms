303a5ed2179618b58cc7549170154928cec53a64
bhankymyk <bankole123michael@gmail.com>
2022-07-19 21:57:09 +0100

    sort h updated

--- sort.h
+++ sort.h
@@ -1,8 +1,8 @@
-#ifndef SORTING_H
-#define SORTING_H
+#ifndef SORT_H
+#define SORT_H
 
 #include <stddef.h>
-
+#include <stdlib.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -29,5 +29,12 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
 #endif
